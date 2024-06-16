7b9ef2d04bdc6f64c6046adfc465da1fb7d03407
unknown <galawoj@gmail.com>
2024-06-16 15:09:07 +0200

    editing style

--- client/src/components/ButtonGenerator/ButtonGenerator.tsx
+++ client/src/components/ButtonGenerator/ButtonGenerator.tsx
@@ -9,7 +9,10 @@ export default function ButtonGenerator() {
       {typing ? (
         <span className={styles.loader}></span>
       ) : (
-        <button onClick={generateHandle}>
+        <button
+          style={{ outline: "none", border: "none" }}
+          onClick={generateHandle}
+        >
           {isFirstText ? "start" : "continue"}
         </button>
       )}
