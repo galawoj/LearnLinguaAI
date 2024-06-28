748d9cc4105c97187b824bb5ab3509cb9aa33f53
unknown <galawoj@gmail.com>
2024-06-28 22:40:29 +0200

    change fetchGptResponse util

--- client/src/api/fetchGptResponse.ts
+++ client/src/api/fetchGptResponse.ts
@@ -3,21 +3,23 @@ import { ApiRequestBodyType } from "../types/ApiRequestBodyType";
 
 
 export async function fetchGptResponse(apiRequestBody:ApiRequestBodyType){
-    return fetch("https://wgservice-apim.azure-api.net/backend/api", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(apiRequestBody),
-      })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok ' + response.statusText);
-        }
-        return response.json();
-      })
-      .catch(error => {
+  try{
+    const response = await fetch("https://wgservice-apim.azure-api.net/backend/api", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(apiRequestBody),
+    })
+
+    if(!response.ok){
+      throw new Error('Error response from API ' + response.statusText);
+    }
+   
+   const data = await response.json()
+   return data
+  }catch(error) {
         console.error('Fetch error:', error);
         throw error;
-      });
+      };
 }
