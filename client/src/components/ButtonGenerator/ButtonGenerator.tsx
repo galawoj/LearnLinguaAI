fc1c706b3f52e111ee39d23a1b1fa0e78cb5231a
unknown <galawoj@gmail.com>
2024-08-22 20:57:10 +0200

    correction in ButtonGenerator component

--- client/src/components/ButtonGenerator/ButtonGenerator.tsx
+++ client/src/components/ButtonGenerator/ButtonGenerator.tsx
@@ -1,18 +1,12 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useAppContext } from "../../store/app-context";
 import styles from "./buttonGenerator.module.scss";
 
 export default function ButtonGenerator() {
   const { typing, isFirstText, generateHandle } = useAppContext();
-  const [isFirstRender, setIsFirstRender] = useState<boolean>(true);
 
   useEffect(() => {
-    if (isFirstRender) {
-      generateHandle();
-      setIsFirstRender(false);
-    } else {
-      return;
-    }
+    generateHandle();
   }, []);
 
   return (
