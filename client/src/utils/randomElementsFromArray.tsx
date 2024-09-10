b29ddfadc62cfc22a0bfff520668d59547a57dac
unknown <galawoj@gmail.com>
2024-09-10 19:45:14 +0200

    cleaner randomElementsFromArray

--- client/src/utils/randomElementsFromArray.tsx
+++ client/src/utils/randomElementsFromArray.tsx
@@ -6,16 +6,14 @@ export default function randomElementsFromArray<T>(
     return array;
   } else {
     let duplicateArray = [...array];
-
     let randArray: T[] = [];
 
     for (let i = 0; i < numberOfRandEl; i++) {
       let randIndex = Math.floor(Math.random() * duplicateArray.length);
-
-      duplicateArray.splice(randIndex, 1);
       randArray.push(duplicateArray[randIndex]);
+      duplicateArray.splice(randIndex, 1);
     }
-
+    console.log(randArray);
     return randArray;
   }
 }
