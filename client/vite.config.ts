2fdf06afa4db552ced105c15cb82fa1fdf0a8df8
unknown <galawoj@gmail.com>
2025-06-22 21:31:44 +0200

    change base server

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,21 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
-  server: {  
-    proxy: { 
-      '/api': {
-        target: 'http://localhost:3000/api', // Adres serwera backendowego
+
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000/api", // Adres serwera backendowego
         changeOrigin: true,
         secure: false,
-         rewrite: (path) => path.replace(/^\/api/, ''), // Opcjonalne, aby usunąć '/api' z przekierowanych żądań
+        rewrite: (path) => path.replace(/^\/api/, ""), // Opcjonalne, aby usunąć '/api' z przekierowanych żądań
       },
     },
   },
- 
- base:'https://lingua-ai.pl/',
 
+  base: "https://galawoj.github.io/LearnLinguaAI_build/",
 });
